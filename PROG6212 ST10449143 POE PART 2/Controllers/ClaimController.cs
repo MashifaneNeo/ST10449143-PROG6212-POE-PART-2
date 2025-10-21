b0b60424b6f55dc53def317cc357c3607a138b75
Neo Ya Gao Mashifane <ST10449143@imconnect.edu.za>
2025-10-21 15:35:36 +0200

    Updated claim submission with POST action and validation

--- PROG6212 ST10449143 POE PART 2/Controllers/ClaimController.cs
+++ PROG6212 ST10449143 POE PART 2/Controllers/ClaimController.cs
@@ -1,10 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PROG6212_ST10449143_POE_PART_1.Models;
 
 namespace PROG_UI_MVC.Controllers
 {
     public class ClaimsController : Controller
-    {
-        public IActionResult Submit() => View();
+    {        
+        public IActionResult Submit()
+        {
+            var model = new ClaimViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Submit(ClaimViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] = "Claim submitted successfully!";
+                return RedirectToAction("Submit");
+            }
+
+            // If validation fails, return to form with errors
+            return View(model);
+        }
+
         public IActionResult ViewClaims() => View();
         public IActionResult Approvals() => View();
         public IActionResult UploadDocs() => View();
