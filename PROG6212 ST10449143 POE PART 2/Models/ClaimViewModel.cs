65ee6f2a43e4e8e795960a531339c944522ec5d9
Neo Ya Gao Mashifane <ST10449143@imconnect.edu.za>
2025-10-21 15:33:58 +0200

    Created ClaimView Model for claim submission data binding

---
+++ PROG6212 ST10449143 POE PART 2/Models/ClaimViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace PROG6212_ST10449143_POE_PART_1.Models
+{
+    public class ClaimViewModel
+    {
+        public string LecturerName { get; set; }
+        public string Month { get; set; }
+        public decimal HoursWorked { get; set; }
+        public decimal HourlyRate { get; set; }
+        public string AdditionalNotes { get; set; }
+        public List<string> AvailableMonths => new List<string>
+        {
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        };
+    }
+}
